9d6ccfe0419d841dd9c49b47fbf5f40f863cdfd9
Tyr Mactire <tyr@pettingzoo.co>
2018-12-23 12:38:32 -0800

    lines and rectangles

--- gfx/__init__.py
+++ gfx/__init__.py
@@ -6,7 +6,7 @@ class GFX:
         self._port = serial.Serial(port, 115200)
 
         self._brightness = 32
-        self._port.write(b'070000\n')
+        self.fill_screen(0, 0, 0)
 
     def color888(self, r, g, b):
         new_r = self._map(r, 0, 255, 0, self._brightness - 1)
@@ -20,9 +20,33 @@ class GFX:
 
         return color
 
+    def draw_fast_h_line(self, x, y, w, r, g, b):
+        color = self.color888(r, g, b)
+        self._port.write("05{0:02x}{1:02x}{2:02x}{3:04x}\n".format(x, y, w, color))
+
+    def draw_fast_v_line(self, x, y, h, r, g, b):
+        color = self.color888(r, g, b)
+        self._port.write("04{0:02x}{1:02x}{2:02x}{3:04x}\n".format(x, y, h, color))
+
+    def draw_line(self, x0, y0, y1, x1, r, g, b):
+        color = self.color888(r, g, b)
+        self._port.write("08{0:02x}{1:02x}{2:02x}{3:02x}{4:04x}\n".format(x0, y0, y1, x1, color))
+
     def draw_pixel(self, x, y, r, g, b):
         color = self.color888(r, g, b)
         self._port.write("01{0:02x}{1:02x}{2:04x}\n".format(x, y, color))
+
+    def draw_rect(self, x, y, w, h, r, g, b):
+        color = self.color888(r, g, b)
+        self._port.write("09{0:02x}{1:02x}{2:02x}{3:02x}{4:04x}\n".format(x, y, w, h, color))
+
+    def fill_rect(self, x, y, w, h, r, g, b):
+        color = self.color888(r, g, b)
+        self._port.write("06{0:02x}{1:02x}{2:02x}{3:02x}{4:04x}\n".format(x, y, w, h, color))
+
+    def fill_screen(self, r, g, b):
+        color = self.color888(r, g, b)
+        self._port.write("07{0:04x}\n".format(color))
 
     def set_brightness(self, b):
         if b < 1:
--- main.py
+++ main.py
@@ -3,4 +3,15 @@ from gfx import GFX
 if __name__ == '__main__':
     panel = GFX("COM21")
 
-    panel.draw_pixel(1, 1, 255, 127, 0)
+    panel.draw_pixel(0, 0, 255, 127, 0)
+    panel.draw_pixel(17, 17, 255, 127, 0)
+
+    panel.draw_fast_v_line(1, 1, 16, 0, 255, 255)
+    panel.draw_fast_h_line(1, 1, 16, 0, 255, 255)
+    panel.draw_fast_v_line(16, 1, 16, 0, 255, 255)
+    panel.draw_fast_h_line(1, 16, 16, 0, 255, 255)
+
+    panel.fill_rect(0, 18, 5, 5, 127, 127, 127)
+    panel.draw_rect(5, 18, 5, 5, 22, 44, 66)
+
+    panel.draw_line(3, 0, 9, 17, 255, 0, 0)
